86e3cee54f4773bb388412a48c88fdac50765fe7
Simon Taranto <simon.taranto@gmail.com>
2013-12-05 18:34:04 -0700

    update

--- ruby/grade-school/school.rb
+++ ruby/grade-school/school.rb
@@ -1,29 +1,21 @@
 class School
 
-  def initialize
-    @db = Hash.new()
-  end
+  attr_reader :db
 
-  def db
-    @db
+  def initialize
+    @db = Hash.new { |h,k| h[k] = [] }
   end
 
   def add(name, grade)
-    if @db[grade].nil?
-      @db[grade] = []
-    end
-    @db[grade].push(name)
+    db[grade].push(name)
   end
 
   def grade(class_num)
-    if @db[class_num].nil?
-      @db[class_num] = []
-    end
-    @db[class_num]
+    db[class_num]
   end
 
   def sort
-    @db.sort_by{ |k,v| k}
+    Hash[db.each_key{|k| db[k] = db[k].sort}.sort]
   end
 
 end
