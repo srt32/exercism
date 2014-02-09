0279ef875e124c654e413c32ca176be3f2e33fbe
Simon Taranto <simon.taranto@gmail.com>
2014-02-09 13:30:40 -0700

    update ruby raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,18 +1,18 @@
 class Raindrops
 
   def convert(number)
-    result = word_num.select { |_, v| v.call(number) }.keys.join
+    result = droplet_noises.select { |_, v| v.call(number) }.keys.join
     result = number.to_s if result.empty?
     result
   end
 
   private
 
-  def word_num
+  def droplet_noises
     {
-      "Pling" => ->(number) { number.divides_evenly_by(3) },
-      "Plang" => ->(number) { number.divides_evenly_by(5) },
-      "Plong" => ->(number) { number.divides_evenly_by(7) }
+      "Pling" => ->(number) { number.divides_evenly_by?(3) },
+      "Plang" => ->(number) { number.divides_evenly_by?(5) },
+      "Plong" => ->(number) { number.divides_evenly_by?(7) }
     }
   end
 
@@ -20,7 +20,7 @@ end
 
 class Fixnum
 
-  def divides_evenly_by(divisor)
+  def divides_evenly_by?(divisor)
     self % divisor == 0
   end
 
