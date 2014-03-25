3e5e35d89eaeb9da0ff0eb9aae42f6779b9cd66f
Simon Taranto <simon.taranto@gmail.com>
2014-03-25 10:45:38 -0700

    working implementation of ruby roman numerals

--- ruby/roman-numerals/roman.rb
+++ ruby/roman-numerals/roman.rb
@@ -1,15 +1,30 @@
 class Fixnum
 
+  DIGITS =
+    {
+      1000 => "M",
+      900 => "CM",
+      500 => "D",
+      400 => "CD",
+      100 => "C",
+      90 => "XC",
+      50 => "L",
+      40 => "XL",
+      10 => "X",
+      9 => "IX",
+      5 => "V",
+      4 => "IV",
+      1 => "I"
+    }
+
   def to_roman
     result = ""
-    if self == 9
-      result << "IX"
-    elsif (5..8).include?(self)
-      result << "V" + "I" * (self - 5)
-    elsif self == 4
-      result << "IV"
-    else
-      result << "I" * self
+    remainder = self
+    DIGITS.each do |dec, char|
+      while remainder >= dec
+        result << char
+        remainder -= dec
+      end
     end
     result
   end
--- ruby/roman-numerals/roman_numerals_test.rb
+++ ruby/roman-numerals/roman_numerals_test.rb
@@ -37,52 +37,42 @@ class RomanTest < MiniTest::Unit::TestCase
   end
 
   def test_48
-    skip
     assert_equal 'XLVIII', 48.to_roman
   end
 
   def test_59
-    skip
     assert_equal 'LIX', 59.to_roman
   end
 
   def test_93
-    skip
     assert_equal 'XCIII', 93.to_roman
   end
 
   def test_141
-    skip
     assert_equal 'CXLI', 141.to_roman
   end
 
   def test_163
-    skip
     assert_equal 'CLXIII', 163.to_roman
   end
 
   def test_402
-    skip
     assert_equal 'CDII', 402.to_roman
   end
 
   def test_575
-    skip
     assert_equal 'DLXXV', 575.to_roman
   end
 
   def test_911
-    skip
     assert_equal 'CMXI', 911.to_roman
   end
 
   def test_1024
-    skip
     assert_equal 'MXXIV', 1024.to_roman
   end
 
   def test_3000
-    skip
     assert_equal 'MMM', 3000.to_roman
   end
 
