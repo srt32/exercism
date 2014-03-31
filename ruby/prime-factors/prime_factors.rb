8aa5ca6f2edd60bf74e63b58b0b16bb168f7a06d
Simon Taranto <simon.taranto@gmail.com>
2014-03-31 08:45:42 -0700

    correct but stack level too deep error

--- ruby/prime-factors/prime_factors.rb
+++ ruby/prime-factors/prime_factors.rb
@@ -1,17 +1,16 @@
 class PrimeFactors
 
-  def self.for(number)
-    divisor = 2
-    prime_factors = []
-    while number > 1
-      if number % divisor == 0
-        prime_factors << divisor
-        number = number / divisor
-      else
-        divisor += 1
-      end
+  def self.for(number, divisor=2, factors=[])
+    return factors if number == 1
+
+    if number % divisor == 0
+      factors.push(divisor)
+      number /= divisor
+      self.for(number, divisor, factors)
+    else
+      divisor += 1
+      self.for(number, divisor, factors)
     end
-    prime_factors
   end
 
 end
