f7e17797ec11ce6470b6bb1bd78124388f1b6c66
Simon Taranto <Simon@SRT.local>
2014-05-06 21:27:38 -0700

    golang raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,5 +1,28 @@
 package raindrops
 
-func Convert(number int) string {
-  return "foo"
+import (
+  "strconv"
+  "unicode/utf8"
+)
+
+func Convert(n int) string {
+  values := map[int]string{
+    3: "Pling",
+    5: "Plang",
+    7: "Plong",
+  }
+
+  result := ""
+
+  for k, v := range values {
+    if n%k == 0 {
+      result = result + v
+    }
+  }
+
+  if utf8.RuneCountInString(result) == 0 {
+    return strconv.Itoa(n)
+  } else {
+    return result
+  }
 }
