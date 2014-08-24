0093710013cbd8a9df6fb10a991390a7ee32184b
Simon Taranto <simon.taranto@gmail.com>
2014-08-24 16:55:22 -0700

    WIP, complete error checking

--- go/queen-attack/queen_attack.go
+++ go/queen-attack/queen_attack.go
@@ -2,13 +2,38 @@ package queenattack
 
 import (
 	"errors"
+	"strconv"
 )
 
 func CanQueenAttack(w string, b string) (attack bool, err error) {
+	white := position{w}
+	black := position{b}
+
 	if (w == b) {
-		err := errors.New("Same square")
-		return false, err
+		return false, errors.New("Same square")
+	} else if white.outOfBounds() {
+		return false, errors.New("Out of bounds")
+	} else if black.outOfBounds() {
+		return false, errors.New("Out of bounds")
 	} else {
+		// b4, b7, handle same file
 	  return false, nil
 	}
 };
+
+type position struct {
+	Location string
+}
+
+func (p position) outOfBounds() bool {
+	location := p.Location
+	rank, err := strconv.ParseInt(string(location[1]), 0, 32)
+	if err != nil {
+		return true
+	}
+	if rank > 8 {
+	  return true
+	} else {
+		return false
+	}
+}
