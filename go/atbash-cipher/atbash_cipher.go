2af4445f7b013dcf6ba82f6e5266e6583e826981
srt32 <simon.taranto@gmail.com>
2014-11-03 08:49:56 -0800

    WIP, go atbash refactoring

--- go/atbash-cipher/atbash_cipher.go
+++ go/atbash-cipher/atbash_cipher.go
@@ -1,68 +1,37 @@
 package atbash
 
 import (
-	"strings"
+	"unicode"
 )
 
 func Atbash(str string) string {
-	var encodedChars []string
-	var count int
+	var encodedMessage []rune
 
-	chars := strings.Split(str, "")
+	for _, r := range str {
+		normalizedR := unicode.ToLower(r)
+		var count int
 
-	for _, char := range chars {
-		encodedChar := mapping(char)
+		encodedR := encodeRune(normalizedR)
 
-		if encodedChar != "" {
+		if encodedR != ' ' {
 			count += 1
-			encodedChars = append(encodedChars, encodedChar)
-		}
+			if count == 5 {
+				encodedMessage = append(encodedMessage, ' ')
+				count = 0
+			}
 
-		if count == 5 {
-			encodedChars = append(encodedChars, " ")
-			count = 0
+			encodedMessage = append(encodedMessage, encodedR)
 		}
 	}
 
-	return strings.TrimSpace(strings.Join(encodedChars, ""))
+	return string(encodedMessage)
 }
 
-func mapping(str string) string {
-	return map[string]string{
-		"a": "z",
-		"b": "y",
-		"c": "x",
-		"d": "w",
-		"e": "v",
-		"f": "u",
-		"g": "t",
-		"h": "s",
-		"i": "r",
-		"j": "q",
-		"k": "p",
-		"l": "o",
-		"m": "n",
-		"n": "m",
-		"o": "l",
-		"p": "k",
-		"q": "j",
-		"r": "i",
-		"s": "h",
-		"t": "g",
-		"u": "f",
-		"v": "e",
-		"w": "d",
-		"x": "c",
-		"y": "b",
-		"z": "a",
-		"1": "1",
-		"2": "2",
-		"3": "3",
-		"4": "4",
-		"5": "5",
-		"6": "6",
-		"7": "7",
-		"8": "8",
-		"9": "9",
-	}[strings.ToLower(str)]
+func encodeRune(r rune) rune {
+	switch {
+	case 'a' <= r && r <= 'z':
+		return 25 - r + 97 + 97
+	default:
+		return r
+	}
 }
