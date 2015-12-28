caf387dec264d6f1f33c6c935830e7551d77db4a
Simon Taranto <simon.taranto@gmail.com>
2015-12-27 17:03:26 -0800

    go pascal correct depth WIP

--- go/pascals-triangle/triangle.go
+++ go/pascals-triangle/triangle.go
@@ -1,5 +1,17 @@
 package pascal
 
 func Triangle(depth int) [][]int {
-	return [][]int{{1}}
+	var triangle [][]int
+
+	for i := 1; i <= depth; i++ {
+		var newLayer = []int{}
+
+		for element := 1; element <= i; element++ {
+			newLayer = append(newLayer, 1)
+		}
+
+		triangle = append(triangle, newLayer)
+	}
+
+	return triangle
 }
